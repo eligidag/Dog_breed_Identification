00f345821440e75f00f4db524bc507f192663ff4
eligidag <114935360+eligidag@users.noreply.github.com>
2022-12-14 21:16:20 +0000

    stored the target/mystery DNA sequence in a list (dog_seq). Calculated the Levenshtein distance between the target DNA and all the sequences in the dog breeds database. Code does not work as intended as all the sequences have the same distance.

--- code/DBI.py
+++ code/DBI.py
@@ -2,6 +2,7 @@ dog_breeds = r'C:\Users\eligi\Desktop\pythonProject\data\dog_breeds.fa'
 target_seq = r'C:\Users\eligi\Desktop\pythonProject\data\mystery.fa'
 
 from collections import defaultdict
+from Levenshtein import distance
 
 dog_breeds_DB = defaultdict(str)
 with open(dog_breeds, "r") as f:
@@ -18,4 +19,12 @@ with open(target_seq, "r") as f:
             key = line.strip('\n')
         else:
             target_seq_dict[key] += line.strip('\n')
-print(target_seq_dict)
+#print(target_seq_dict)
+
+
+dog_seq = []
+key, values = next(iter(target_seq_dict.items()))
+dog_seq.append(values)
+for key, value in dog_breeds_DB.items():
+    dist = distance(dog_seq, value)
+    print(dist)
