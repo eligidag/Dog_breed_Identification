fd928feab1ac19d37f92e91160c6d9e1f56024a8
eligidag <114935360+eligidag@users.noreply.github.com>
2022-12-14 12:38:46 +0000

    Converted dog_breeds.fa into a dictionary named dog_breeds_DB (DB = data base), where lines starting with ">" are used as keys and everything that follows after is stored as a value until it encounters new ">".

--- code/DBI.py
+++ code/DBI.py
@@ -1,3 +1,13 @@
 dog_breeds = r'C:\Users\eligi\Desktop\pythonProject\data\dog_breeds.fa'
 target_seq = r'C:\Users\eligi\Desktop\pythonProject\data\mystery.fa'
 
+from collections import defaultdict
+
+dog_breeds_DB = defaultdict(str)
+with open(dog_breeds, "r") as f:
+    for line in f:
+        if line[0] == '>':
+            key = line.strip('\n')
+        else:
+            dog_breeds_DB[key] += line.strip('\n')
+print(dog_breeds_DB)
