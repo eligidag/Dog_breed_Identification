721c752aebe1a5a4226d727040033d97457f1772
eligidag <114935360+eligidag@users.noreply.github.com>
2022-12-18 16:34:50 +0000

    Deleted the previous code that was meant to look for the most similar breed as it was not working properly. New code is working as intended. The new code iterates over keys and values in both dictionaries and calculates the distance between target sequence and each breed sequence using "distance" function. Key of the most similar sequence is stored inside "most_similar" variable

--- code/DBI.py
+++ code/DBI.py
@@ -19,12 +19,22 @@ with open(target_seq, "r") as f:
             key = line.strip('\n')
         else:
             target_seq_dict[key] += line.strip('\n')
-#print(target_seq_dict)
 
-
-dog_seq = []
-key, values = next(iter(target_seq_dict.items()))
-dog_seq.append(values)
-for key, value in dog_breeds_DB.items():
-    dist = distance(dog_seq, value)
-    print(dist)
+#empty variable used to store minimum distance
+min_distance = float('inf')
+#variable to store the key of the most similar sequence
+most_similar = None
+#iteration over the keys and values in the target_seq_dict
+for target_key, target_value in target_seq_dict.items():
+    #iteration over the keys and values in the dog_breeds_DB
+    for breed_key, breed_value in dog_breeds_DB.items():
+        #calculating the Levenshtein distance between mystery/target sequence and all the sequences in the DB
+        dist = distance(target_value, breed_value)
+        #if the current distance is smaller than the one that is currently stored in minimum distance
+        #it becomes the new current minimum distance
+        if dist < min_distance:
+            #updates minimum distance based on "if" results
+            min_distance = dist
+            #stores the key value pair that is currently the most similar to the target seq
+            most_similar = breed_key
+print(most_similar)
