8731d807526f2fa920211dc13cf9bb5ec5f3beb1
eligidag <114935360+eligidag@users.noreply.github.com>
2022-12-18 17:00:33 +0000

    Put the previous loose code into functions - read_fasta() and find_most_similar()

--- code/DBI.py
+++ code/DBI.py
@@ -4,37 +4,44 @@ target_seq = r'C:\Users\eligi\Desktop\pythonProject\data\mystery.fa'
 from collections import defaultdict
 from Levenshtein import distance
 
-dog_breeds_DB = defaultdict(str)
-with open(dog_breeds, "r") as f:
-    for line in f:
-        if line[0] == '>':
-            key = line.strip('\n')
-        else:
-            dog_breeds_DB[key] += line.strip('\n')
+def read_fasta(filename):
+    """Read a FASTA file and return a dictionary with the keys and values."""
+    data = defaultdict(str)
+    with open(filename, "r") as f:
+        for line in f:
+            if line[0] == '>':
+                key = line.strip('\n')
+            else:
+                data[key] +=line.strip("\n")
+    return data
 
-target_seq_dict = defaultdict(str)
-with open(target_seq, "r") as f:
-    for line in f:
-        if line[0] == '>':
-            key = line.strip('\n')
-        else:
-            target_seq_dict[key] += line.strip('\n')
+def find_most_similar(target_seq_dict, dog_breeds_dict):
+    """ Find the most similar sequence in the dog breeds dictionary to the target sequence,
+     using the Levenshtein distance"""
 
-#empty variable used to store minimum distance
-min_distance = float('inf')
-#variable to store the key of the most similar sequence
-most_similar = None
-#iteration over the keys and values in the target_seq_dict
-for target_key, target_value in target_seq_dict.items():
-    #iteration over the keys and values in the dog_breeds_DB
-    for breed_key, breed_value in dog_breeds_DB.items():
-        #calculating the Levenshtein distance between mystery/target sequence and all the sequences in the DB
-        dist = distance(target_value, breed_value)
-        #if the current distance is smaller than the one that is currently stored in minimum distance
-        #it becomes the new current minimum distance
-        if dist < min_distance:
-            #updates minimum distance based on "if" results
-            min_distance = dist
-            #stores the key value pair that is currently the most similar to the target seq
-            most_similar = breed_key
+    # empty variable used to store minimum distance
+    min_distance = float('inf')
+    # variable to store the key of the most similar sequence
+    most_similar = None
+    # iteration over the keys and values in the target_seq_dict
+    for target_key, target_value in target_seq_dict.items():
+        # iteration over the keys and values in the dog_breeds_DB
+        for breed_key, breed_value in dog_breeds_DB.items():
+            # calculating the Levenshtein distance between mystery/target sequence and all the sequences in the DB
+            dist = distance(target_value, breed_value)
+            # if the current distance is smaller than the one that is currently stored in minimum distance
+            # it becomes the new current minimum distance
+            if dist < min_distance:
+                # updates minimum distance based on "if" results
+                min_distance = dist
+                # stores the key value pair that is currently the most similar to the target seq
+                most_similar = breed_key
+    return most_similar
+
+#read the FASTA files into dictionaries
+dog_breeds_DB = read_fasta(dog_breeds)
+target_seq_dict = read_fasta(target_seq)
+
+#find the most similar sequence
+most_similar = find_most_similar(target_seq_dict,dog_breeds_DB)
 print(most_similar)
