3b4862bb0259a6517ab0619772cd1103ba2cab32
eligidag <114935360+eligidag@users.noreply.github.com>
2022-12-19 21:46:07 +0000

    Updated find_most_similar() to return only name of the breed and length_differenc, without additional sequence information

--- code/DBI.py
+++ code/DBI.py
@@ -30,36 +30,33 @@ def find_most_similar(target_seq_dict, dog_breeds_DB):
     # variable to store the key of the most similar sequence
     most_similar = None
     #variable to store the length difference between the most similar sequence and the target sequence
-    length_diff = 0
     # iteration over the keys and values in the target_seq_dict
     for target_key, target_value in target_seq_dict.items():
         # iteration over the keys and values in the dog_breeds_DB
         for breed_key, breed_value in dog_breeds_DB.items():
             # calculating the Levenshtein distance between mystery/target sequence and all the sequences in the DB
             dist = distance(target_value, breed_value)
+            # Calculate the length difference between the target sequence and the current sequence
+            len_diff = abs(len(target_value) - len(breed_value))
             # if the current distance is smaller than the one that is currently stored in minimum distance
             # it becomes the new current minimum distance
             if dist < min_distance:
                 # updates minimum distance based on "if" results
                 min_distance = dist
-                # stores the key value pair that is currently the most similar to the target seq
                 most_similar = breed_key
-                #store the length difference between the most similar sequence and the target seq
-                length_diff = abs(len(target_value)) - len(breed_value)
+                breed_name = breed_key[breed_key.index("breed=")+len("breed="):breed_key.index("]", breed_key.index("breed="))]
 
-    return most_similar, length_diff
-
-
-
+    return (breed_name, min_distance)
 
 # read the FASTA files into dictionaries
 dog_breeds_DB = read_fasta(dog_breeds)
 target_seq_dict = read_fasta(target_seq)
 
-# find the most similar sequence
-most_similar, length_diff = find_most_similar(target_seq_dict, dog_breeds_DB)
-print(f"Information about the most similar breed: {most_similar}")
-print(f"The length difference is: {length_diff}")
+# find the most similar breed and the length difference
+most_similar_breed, length_difference = find_most_similar(target_seq_dict, dog_breeds_DB)
+
+print(f"The mystery sequence is the most similar to the one of {most_similar_breed}")
+print(f"With the sequence length difference of: {length_difference}")
 
 
 
